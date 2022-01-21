87a87c19177933d94c6ce3a7773528710ccab81d
BariVakhidov <memeshuntergod@gmail.com>
2022-01-22 00:50:09 +0300

    test commit 3

--- src/app/AppWrapper.tsx
+++ src/app/AppWrapper.tsx
@@ -4,7 +4,7 @@ import { appActionCreators } from '@/redux/app/action-creators';
 import { App } from '@/app/App';
 import { Spin } from 'antd';
 import { modelsActionCreators } from '@/redux/models/action-creators';
-
+//3
 export const AppWrapper: FC = memo(() => {
 
   const { initialized, user } = useAppSelector(state => state.app);
